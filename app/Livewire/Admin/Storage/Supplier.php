b934ea73391f951c825610bc8357e806bd18d748
Muhamad Luthfi Novianto <muhamadupi54@gmail.com>
2025-11-23 09:12:25 +0700

    feat: Add supplier crud menu

---
+++ app/Livewire/Admin/Storage/Supplier.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Storage;
+
+use Livewire\Component;
+
+class Supplier extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.storage.supplier');
+    }
+}
